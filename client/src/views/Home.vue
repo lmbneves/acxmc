7eef2aab928d4761e743efb783b0049809ce83d2
lmbneves <lmbneves@gmail.com>
2021-03-25 19:40:19 -0400

    add main vis to home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Home</h1>
+  <div class="mainvisual">
+    <div class="mainvisual-bg"></div>
+  </div>
 </template>
+
+<style scoped>
+.mainvisual {
+  width: 100%;
+  height: 100vh;
+}
+
+.mainvisual-bg {
+  width: 100%;
+  height: 100%;
+  background: url("~@/assets/images/acxmc.jpg") center center no-repeat;
+  background-size: cover;
+}
+</style>
