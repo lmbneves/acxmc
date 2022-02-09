f185e7dd0b81ff4318c598cba40df28937989231
lmbneves <lmbneves@gmail.com>
2022-02-08 20:38:09 -0500

    draft1 skeleton

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="mainvisual">
-    <div class="mainvisual-bg"></div>
+    <div class="main-image">
+      <img src="@/assets/images/abby-alone-rotate.png">
+    </div>
+    <div class="name-container">
+      <div class="name">
+        <h2>Abagael</h2>
+        <h1>Cheng</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +16,44 @@
 .mainvisual {
   width: 100%;
   height: 100vh;
+  position: relative;
 }
 
-.mainvisual-bg {
+.main-image {
   width: 100%;
   height: 100%;
-  background: url("~@/assets/images/acxmc.jpg") center center no-repeat;
-  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 5;
+}
+
+.main-image img {
+  height: 100%;
+  padding-right: 45%;
+}
+
+.name-container {
+  width: 50%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.name {
+  padding: 80px;
+  text-align: left;
+}
+
+.name h1 {
+  margin: 0;
+  font-size: 160pt;
+  padding-left: 40px;
+}
+
+.name h2 {
+  margin: 0;
+  font-size: 120pt;
+  margin-bottom: -80px;
 }
 </style>
