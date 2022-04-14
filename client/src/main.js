ce651150a1fb89663fe17d68b858b7819d63a5e1
lmbneves <lmbneves@gmail.com>
2022-04-13 23:49:34 -0400

    add masonry to media

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import MasonryWall from '@yeger/vue-masonry-wall'
 
 var app = createApp(App)
 
+app.use(MasonryWall)
 app.use(router)
 
 app.mount('#app')
